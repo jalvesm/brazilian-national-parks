176c96ddea55baa7fc1d0544c2c5480b1ed10ae0
joana <joanaalvesm@gmail.com>
2023-11-26 11:35:36 -0300

    added map and changed directory's structure

--- scripts/main.js
+++ scripts/main.js
@@ -44,10 +44,9 @@ async function renderPage() {
 }
 
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9hbmFhbHZlc20iLCJhIjoiY2xwZmh0bDcwMWJ3MTJqcXN2MGhlOWFkcSJ9.zLCmGObcm88gLI6twBZnmQ';
+mapboxgl.accessToken = 'key goes here';
 const map = new mapboxgl.Map({
-container: 'map', // container ID
-// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+container: 'map',
 style: 'mapbox://styles/mapbox/streets-v12', // style URL
 center: [-74.5, 40], // starting position [lng, lat]
 zoom: 9 // starting zoom
